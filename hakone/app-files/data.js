81ed34f8d956f4176faa767e1ce390de56b8c26f
weii9763 <wadechou0715@gmail.com>
2024-03-27 14:53:43 +0800

    hakone

---
+++ hakone/app-files/data.js
@@ -0,0 +1,317 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "讀賣新聞大樓",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1448,
+      "initialViewParameters": {
+        "yaw": -0.12052119510966719,
+        "pitch": -0.39656254449667827,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18483087382693952,
+          "pitch": 0.07755941701347169,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.46895726428223306,
+          "pitch": 0.17426692720112058,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.076155313973997,
+          "pitch": -0.2512056813932393,
+          "title": "Title",
+          "text": "為讀賣新聞總部，抑是驛傳的起點"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "鶴見中繼所",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4086104480962476,
+        "pitch": -0.12785524738935372,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07373925473211429,
+          "pitch": 0.1936591920751649,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.72551847136714,
+          "pitch": 0.25662726371319877,
+          "rotation": 7.0685834705770345,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5195506937542209,
+          "pitch": -0.35569446328072374,
+          "title": "Title",
+          "text": "為區間二的起點"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "權太坂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0033077646200006,
+        "pitch": 0.1278095777879109,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1255565671353818,
+          "pitch": 0.21173014724488937,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.44587832618143963,
+          "pitch": 0.09233274713270845,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1203694321887774,
+          "pitch": -0.09235875664136195,
+          "title": "Title",
+          "text": "為區間二最考驗選手實力的其中一個路段"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "平塚中繼所",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0901587183913852,
+        "pitch": 0.036986435280145,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6225399003772916,
+          "pitch": 0.16599067419774904,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.1540850958549953,
+          "pitch": 0.13097931747523717,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4467232099286651,
+          "pitch": -0.37856120681985317,
+          "title": "Title",
+          "text": "區間四的起點"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "箱根蘆之湖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.207121496719328,
+        "pitch": -0.3281147036299963,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9060803398347197,
+          "pitch": 0.07781122405635976,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.2790720013757824,
+          "pitch": 0.3877317198594916,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3158535917443928,
+          "pitch": -0.4181180691491768,
+          "title": "Title",
+          "text": "蘆之湖中，由平地爬升至此的區間五被稱為魔王區間"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "遊行寺",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5413418223967827,
+        "pitch": -0.13978563958760049,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2518274928702766,
+          "pitch": 0.20788109237374996,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.891395942671849,
+          "pitch": 0.21756517571911616,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.07103242435411516,
+          "pitch": -0.21758460807199675,
+          "title": "Title",
+          "text": "為區間八的上坡路段"
+        }
+      ]
+    }
+  ],
+  "name": "Hakone",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
